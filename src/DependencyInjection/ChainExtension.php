26502ccf192052604dbfd607e9da7684a824e0da
Michael Albrecht <m.albrecht@neusta.de>
2025-06-03 11:25:54 +0200

    load configuration for clients correctly improve test coverage for ChainExecutor

--- src/DependencyInjection/ChainExtension.php
+++ src/DependencyInjection/ChainExtension.php
@@ -3,14 +3,18 @@
 namespace Mike4Git\ChainBundle\DependencyInjection;
 
 use Mike4Git\ChainBundle\Attribute\AsChainHandler;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ChildDefinition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class ChainExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
+        $loader = new YamlFileLoader($container, new FileLocator(\dirname(__DIR__, 2) . '/config'));
+        $loader->load('services.yaml');
         $container->registerAttributeForAutoconfiguration(
             AsChainHandler::class,
             static function (ChildDefinition $childDefinition, AsChainHandler $attribute): void {
