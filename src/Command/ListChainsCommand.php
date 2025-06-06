41d0b5522c39eb08cdae98300073bc233efa217f
Michael Albrecht <m.albrecht@neusta.de>
2025-06-06 13:46:48 +0200

    [Task Description] cs-fixed and phpstan fixed

--- src/Command/ListChainsCommand.php
+++ src/Command/ListChainsCommand.php
@@ -47,7 +47,7 @@ class ListChainsCommand extends Command
         $reflection = new \ReflectionClass($this->registry);
         $property = $reflection->getProperty('handlers');
         $property->setAccessible(true);
-        /** @var array<string, array<int, array{handler: object, id: string, priority: int}>> $handlers */
+        /** @var array<string, array<int, array{handler: object, id: string, description: string, priority: int}>> $handlers */
         $handlers = $property->getValue($this->registry);
 
         if (empty($handlers)) {
